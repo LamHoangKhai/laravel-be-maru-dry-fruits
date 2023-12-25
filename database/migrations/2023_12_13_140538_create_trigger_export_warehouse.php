2852b87c51ae9a96b7c32052a368de2a4a62c824
Huan <huanbeu555@gmail.com>
2023-12-26 01:16:02 +0700

    26/12

--- database/migrations/2023_12_13_140538_create_trigger_export_warehouse.php
+++ database/migrations/2023_12_13_140538_create_trigger_export_warehouse.php
@@ -27,7 +27,7 @@ return new class extends Migration
 	    	SET MESSAGE_TEXT = "Error: Expiration date cannot be in the past.";
 	    END IF;
    
-	   IF NEW.transaction_type = 2 THEN
+	   	IF NEW.transaction_type = 2 THEN
 	   		IF NOT EXISTS(SELECT 1 FROM warehouse WHERE (transaction_type = 1 AND shipment = NEW.shipment AND product_id = NEW.product_id)) THEN
 	   			SIGNAL SQLSTATE "45000"
     			SET MESSAGE_TEXT = "Error: Shipment does not exist.";
@@ -44,30 +44,30 @@ return new class extends Migration
             END IF;
 
     		
-	   	IF(NEW.quantity > (SELECT stock_quantity FROM products WHERE id = NEW.product_id)) THEN
-	   		SIGNAL SQLSTATE "45000"
-		      SET MESSAGE_TEXT = "Error: Export need to equal or smaller than stock quantity.";
-	   	ELSEIF(NEW.quantity > (SELECT MIN(current_quantity) FROM warehouse WHERE(shipment = NEW.shipment AND product_id = NEW.product_id) LIMIT 1)) THEN
-	   		SIGNAL SQLSTATE "45000"
-		      SET MESSAGE_TEXT = "Error: Export need to equal or smaller than stock quantity.";
-	    END IF;
+			IF(NEW.quantity > (SELECT stock_quantity FROM products WHERE id = NEW.product_id)) THEN
+				SIGNAL SQLSTATE "45000"
+				SET MESSAGE_TEXT = "Error: Export need to equal or smaller than stock quantity.";
+			ELSEIF(NEW.quantity > (SELECT MIN(current_quantity) FROM warehouse WHERE(shipment = NEW.shipment AND product_id = NEW.product_id) LIMIT 1)) THEN
+				SIGNAL SQLSTATE "45000"
+				SET MESSAGE_TEXT = "Error: Export need to equal or smaller than stock quantity.";
+			END IF;
 
-		IF (SELECT MIN(current_quantity) FROM warehouse WHERE transaction_type = 2 AND shipment = NEW.shipment AND product_id = NEW.product_id LIMIT 1) IS NOT NULL THEN
-			SET NEW.current_quantity = 
-				(SELECT MIN(current_quantity) FROM warehouse WHERE transaction_type = 2 AND shipment = NEW.shipment AND product_id = NEW.product_id LIMIT 1) - NEW.quantity;
-			UPDATE products
-				SET products.store_quantity = products.store_quantity + NEW.quantity,
-					 products.stock_quantity = stock_quantity - NEW.quantity
-	         WHERE products.id = NEW.product_id;
+			IF (SELECT MIN(current_quantity) FROM warehouse WHERE transaction_type = 2 AND shipment = NEW.shipment AND product_id = NEW.product_id LIMIT 1) IS NOT NULL THEN
+				SET NEW.current_quantity = 
+					(SELECT MIN(current_quantity) FROM warehouse WHERE transaction_type = 2 AND shipment = NEW.shipment AND product_id = NEW.product_id LIMIT 1) - NEW.quantity;
+				UPDATE products
+					SET products.store_quantity = products.store_quantity + NEW.quantity,
+						products.stock_quantity = stock_quantity - NEW.quantity
+				WHERE products.id = NEW.product_id;
 
-		ELSE
-		SET NEW.current_quantity = 
-		(SELECT current_quantity FROM warehouse WHERE transaction_type = 1 AND shipment = NEW.shipment AND product_id = NEW.product_id LIMIT 1) - NEW.quantity;
-		UPDATE products
-			SET products.store_quantity = products.store_quantity + NEW.quantity,
-				products.stock_quantity = stock_quantity - NEW.quantity
-		WHERE products.id = NEW.product_id;
-		END IF;
+			ELSE
+				SET NEW.current_quantity = 
+				(SELECT current_quantity FROM warehouse WHERE transaction_type = 1 AND shipment = NEW.shipment AND product_id = NEW.product_id LIMIT 1) - NEW.quantity;
+				UPDATE products
+					SET products.store_quantity = products.store_quantity + NEW.quantity,
+						products.stock_quantity = stock_quantity - NEW.quantity
+				WHERE products.id = NEW.product_id;
+			END IF;
 		END IF;
 	END;
         ');
