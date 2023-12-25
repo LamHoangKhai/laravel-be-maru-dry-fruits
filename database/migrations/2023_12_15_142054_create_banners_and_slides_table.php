da749834766e8936be822a865322e26ae941b812
Huan <huanbeu555@gmail.com>
2023-12-25 20:34:01 +0700

    25/12

--- database/migrations/2023_12_15_142054_create_banners_and_slides_table.php
+++ database/migrations/2023_12_15_142054_create_banners_and_slides_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('image');
             $table->text('description');
-            $table->tinyInteger('position')->comment('1: Slide - 2: Banner');
+            $table->tinyInteger('position')->comment('1: Slide - 2: Parallax Banner - 3: Normal Banner');
             $table->tinyInteger('status')->default(2)->comment('1: Show - 2: Hidden');
             $table->timestamps();
         });
