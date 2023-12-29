3363a1300d617324dffab08ed3f23e697e205123
lamhoangkhai <heoloveninh2014@gmail.com>
2023-12-29 19:16:25 +0700

    29/12

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('remote')) {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
         Paginator::useBootstrapFive();
