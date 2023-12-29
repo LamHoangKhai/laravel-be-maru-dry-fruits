e0d7e3e5d1ecee02470acffbdee17cc481fda544
lamhoangkhai <heoloveninh2014@gmail.com>
2023-12-29 18:18:35 +0700

    29/12

--- resources/views/admin/modules/user/index.blade.php
+++ resources/views/admin/modules/user/index.blade.php
@@ -1,15 +1,15 @@
 @extends('admin.master')
 @push('js')
-    <script src="{{ asset('administrator/plugins/simple-bootstrap-paginator-master/simple-bootstrap-paginator.js') }}">
+    <script src="{{  secure_asset('administrator/plugins/simple-bootstrap-paginator-master/simple-bootstrap-paginator.js') }}">
     </script>
 @endpush
 
 @push('handlejs')
-    <script src="{{ asset('administrator/js/user/main.js') }}" type="module"></script>
+    <script src="{{  secure_asset('administrator/js/user/main.js') }}" type="module"></script>
 @endpush
 
 @push('css')
-    <link rel="stylesheet" href="{{ asset('administrator/css/dropdown-menu-filter.css') }}">
+    <link rel="stylesheet" href="{{  secure_asset('administrator/css/dropdown-menu-filter.css') }}">
 @endpush
 
 @section('content')
--- resources/views/admin/partials/foot.blade.php
+++ resources/views/admin/partials/foot.blade.php
@@ -1,22 +1,22 @@
 <!-- plugin JS -->
-<script src="{{ asset('administrator/plugins/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('administrator/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('administrator/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+<script src="{{  secure_asset('administrator/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{  secure_asset('administrator/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{  secure_asset('administrator/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
 
 <!-- build:js administrator/assets/vendor/js/core.js -->
-<script src="{{ asset('administrator/assets/vendor/libs/jquery/jquery.js') }}"></script>
-<script src="{{ asset('administrator/assets/vendor/libs/popper/popper.js') }}"></script>
-<script src="{{ asset('administrator/assets/vendor/js/bootstrap.js') }}"></script>
-<script src="{{ asset('administrator/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-<script src="{{ asset('administrator/assets/vendor/js/menu.js') }}"></script>
+<script src="{{  secure_asset('administrator/assets/vendor/libs/jquery/jquery.js') }}"></script>
+<script src="{{  secure_asset('administrator/assets/vendor/libs/popper/popper.js') }}"></script>
+<script src="{{  secure_asset('administrator/assets/vendor/js/bootstrap.js') }}"></script>
+<script src="{{  secure_asset('administrator/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+<script src="{{  secure_asset('administrator/assets/vendor/js/menu.js') }}"></script>
 <!-- endbuild -->
 
 <!-- Vendors JS -->
-<script src="{{ asset('administrator/assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+<script src="{{  secure_asset('administrator/assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
 
 
 <!-- Main JS -->
-<script src="{{ asset('administrator/assets/js/main.js') }}"></script>
+<script src="{{  secure_asset('administrator/assets/js/main.js') }}"></script>
 
 
 
