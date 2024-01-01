289d6ffbcf1a6afe64311ee58dcf147023bbfc92
lamhoangkhai <heoloveninh2014@gmail.com>
2024-01-01 21:21:33 +0700

    1/1

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -48,14 +48,14 @@ class OrderController extends Controller
             $order->save();
 
             $info_user = User::where('id', auth('api')->user()->id)->first();
-            if($info_user->full_name == 'Wait for update'|| !$info_user->address = 'Wait for update' || !$info_user->phone = 'Wait for update') {
+            if (!$info_user->full_name || !$info_user->address || !$info_user->phone) {
                 $update_info = [
                     'address' => $request->address,
                     'phone' => $request->phone,
                     'full_name' => $request->full_name
                 ];
                 User::where('id', auth('api')->user()->id)->update($update_info);
-            } 
+            }
             // Realtime notification
             event(new UserOrder($order));
 
